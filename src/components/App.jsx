14be424fa4df0db88d1eb382ce802acfd959f38f
AlexHerbin <101976159+AlexHerbin@users.noreply.github.com>
2022-10-19 21:51:36 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,5 @@
 import { Profile } from 'components/Profile/Profile';
 import { Statistics } from 'components/Statistics/Statistics';
-import { RenderColor } from 'components/Statistics/RenderColor';
 import user from 'user.json';
 import data from 'data.json';
 
